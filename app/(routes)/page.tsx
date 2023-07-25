21e09ef401a25c14cfecd3c53a242e5a8251c1d7
David Castellanos <davcastellanoslarios@gmail.com>
2023-07-25 13:55:29 -0600

    billboard working

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,19 @@
+import { Container } from "@/components/ui/container";
+import { Billboard } from '@/components/billboard';
+import getBillboards from "@/actions/get-billboard";
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+  const billboard = await getBillboards("52499a50-1dfb-4b86-9dd9-5f0834211955")
+
   return (
-    <div>Hello store</div>
+    <Container>
+      <div className="pace-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   )
 }
 
